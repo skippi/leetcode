f3c8d01251ad237f3256e000da4ad83f002f92ac
skippi <jbtcao@gmail.com>
2020-06-06 21:39:16 -0500

    Added brute force two sum solution

--- src/two_sum.rs
+++ src/two_sum.rs
@@ -1,5 +1,18 @@
 #[allow(dead_code)]
-fn two_sum(_nums: Vec<i32>, _target: i32) -> Vec<i32> {
+fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    for (i, &x) in nums.iter().enumerate() {
+        let result = nums
+            .iter()
+            .enumerate()
+            .skip(1)
+            .find(|&(j, &y)| x + y == target && i != j)
+            .map(|(j, _)| vec![i as i32, j as i32]);
+
+        if let Some(indices) = result {
+            return indices;
+        }
+    }
+
     vec![]
 }
 
@@ -10,5 +23,7 @@ mod tests {
     #[test]
     fn test_two_sum() {
         assert_eq!(two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
+        assert_eq!(two_sum(vec![3, 2, 4], 6), vec![1, 2]);
+        assert_eq!(two_sum(vec![2, 5, 5, 11], 10), vec![1, 2]);
     }
 }
