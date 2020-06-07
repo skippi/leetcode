fb22830eb206e44d2c5b04229762544229e0219a
skippi <jbtcao@gmail.com>
2020-06-07 17:33:16 -0500

    Modified nix expr for RLS support

--- shell.nix
+++ shell.nix
@@ -1,6 +1,19 @@
 {
-  pkgs ? import <nixpkgs> {}
+  mozillaOverlay ? import (
+    builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz
+  )
 }:
-with pkgs; mkShell {
-  buildInputs = [rustc cargo rustfmt];
+let
+  nixpkgs = import <nixpkgs> { overlays = [ mozillaOverlay ]; };
+  rustChannel = nixpkgs.latest.rustChannels.stable.rust.override {
+    extensions = [
+      "rust-src"
+      "rls-preview"
+      "clippy-preview"
+      "rustfmt-preview"
+    ];
+  };
+in
+with nixpkgs; mkShell {
+  buildInputs = [cargo rustChannel rustc rustfmt rustup rls];
 }
