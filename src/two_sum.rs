6664bb26a0f7a954a8b0d145bb1b468b7e36f4eb
skippi <jbtcao@gmail.com>
2020-06-06 22:20:01 -0500

    Added optimized two_sum solution

--- src/two_sum.rs
+++ src/two_sum.rs
@@ -1,5 +1,24 @@
+use std::collections::HashMap;
+
 #[allow(dead_code)]
 fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut num_indices: HashMap<i32, usize> = HashMap::new();
+
+    for (i, &x) in nums.iter().enumerate() {
+        let result = num_indices.get(&(target - x)).filter(|&&j| i != j);
+
+        if let Some(&j) = result {
+            return vec![j as i32, i as i32];
+        } else {
+            num_indices.insert(x, i);
+        }
+    }
+
+    vec![]
+}
+
+#[allow(dead_code)]
+fn two_sum_naive(nums: Vec<i32>, target: i32) -> Vec<i32> {
     for (i, &x) in nums.iter().enumerate() {
         let result = nums
             .iter()
@@ -19,6 +38,13 @@ fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_two_sum_naive() {
+        assert_eq!(two_sum_naive(vec![2, 7, 11, 15], 9), vec![0, 1]);
+        assert_eq!(two_sum_naive(vec![3, 2, 4], 6), vec![1, 2]);
+        assert_eq!(two_sum_naive(vec![2, 5, 5, 11], 10), vec![1, 2]);
+    }
 
     #[test]
     fn test_two_sum() {
