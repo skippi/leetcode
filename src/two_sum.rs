41004e3ddbf754d0a378e3f262a694a1b7563ba2
skippi <jbtcao@gmail.com>
2020-06-21 15:30:18 -0500

    Refactored two_sum tests

--- src/two_sum.rs
+++ src/two_sum.rs
@@ -38,18 +38,41 @@ fn two_sum_naive(nums: Vec<i32>, target: i32) -> Vec<i32> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::collections::HashSet;
+
+    macro_rules! set {
+        [ $($x:expr),*] => {
+                {
+                    let mut temp_set = HashSet::new();
+                    $(
+                        temp_set.insert($x);
+                    )*
+                    temp_set
+                }
+        };
+    }
 
     #[test]
     fn test_two_sum_naive() {
-        assert_eq!(two_sum_naive(vec![2, 7, 11, 15], 9), vec![0, 1]);
-        assert_eq!(two_sum_naive(vec![3, 2, 4], 6), vec![1, 2]);
-        assert_eq!(two_sum_naive(vec![2, 5, 5, 11], 10), vec![1, 2]);
+        two_sum_tests(two_sum_naive);
     }
 
     #[test]
     fn test_two_sum() {
-        assert_eq!(two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
-        assert_eq!(two_sum(vec![3, 2, 4], 6), vec![1, 2]);
-        assert_eq!(two_sum(vec![2, 5, 5, 11], 10), vec![1, 2]);
+        two_sum_tests(two_sum);
+    }
+
+    fn two_sum_tests<F>(fun: F)
+    where
+        F: Fn(Vec<i32>, i32) -> Vec<i32>,
+    {
+        assert_eq!(to_set(&fun(vec![2, 7, 11, 15], 9)), set![0, 1]);
+        assert_eq!(to_set(&fun(vec![3, 2, 4], 6)), set![1, 2]);
+        assert_eq!(to_set(&fun(vec![2, 5, 5, 11], 10)), set![1, 2]);
+        assert_eq!(to_set(&fun(vec![0, 4, 3, 0], 0)), set![0, 3]);
+    }
+
+    fn to_set(s: &[i32]) -> HashSet<i32> {
+        s.iter().cloned().collect()
     }
 }
