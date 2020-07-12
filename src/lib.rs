7f0d35ffbccffb4f3e1dabea8f501af9148e8fbb
skippi <jbtcao@gmail.com>
2020-07-12 16:14:24 -0500

    Removed smoke test from lib module

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,3 @@ mod longest_substring_without_repeating_characters;
 mod merge_two_binary_trees;
 mod two_sum;
 mod valid_parentheses;
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
