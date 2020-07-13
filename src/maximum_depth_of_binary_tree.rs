74173d7ce6043f80de61b8555abf159c9624d3e4
skippi <jbtcao@gmail.com>
2020-07-13 18:14:24 -0500

    Added iterative max_depth

--- src/maximum_depth_of_binary_tree.rs
+++ src/maximum_depth_of_binary_tree.rs
@@ -1,5 +1,6 @@
-use std::cmp;
 use std::cell::RefCell;
+use std::cmp;
+use std::collections::VecDeque;
 use std::rc::Rc;
 
 #[derive(Debug, PartialEq, Eq)]
@@ -34,14 +35,49 @@ fn max_depth(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
     }
 }
 
+#[allow(dead_code)]
+fn max_depth_iterative(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+    if root.is_none() {
+        return 0;
+    }
+    let mut queue: VecDeque<Rc<RefCell<TreeNode>>> = VecDeque::new();
+    queue.push_back(root.unwrap());
+    let mut depth = 0;
+    while queue.len() > 0 {
+        let mut level_node_count = queue.len();
+        depth += 1;
+        while level_node_count > 0 {
+            let node = queue.pop_front().unwrap();
+            let mut borrowed = node.borrow_mut();
+            if let Some(left) = borrowed.left.take() {
+                queue.push_back(left);
+            }
+            if let Some(right) = borrowed.right.take() {
+                queue.push_back(right);
+            }
+            level_node_count -= 1;
+        }
+    }
+    depth
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_max_depth() {
-        assert_eq!(max_depth(make_tree(1, 2, 3)), 2);
-        assert_eq!(max_depth(Some(Rc::new(RefCell::new(TreeNode::new(50))))), 1);
+        run_tests(max_depth);
+    }
+
+    #[test]
+    fn test_max_depth_iterative() {
+        run_tests(max_depth_iterative);
+    }
+
+    fn run_tests<F: Fn(Option<Rc<RefCell<TreeNode>>>) -> i32>(fun: F) {
+        assert_eq!(fun(make_tree(1, 2, 3)), 2);
+        assert_eq!(fun(Some(Rc::new(RefCell::new(TreeNode::new(50))))), 1);
     }
 
     fn make_tree(middle: i32, left: i32, right: i32) -> Option<Rc<RefCell<TreeNode>>> {
