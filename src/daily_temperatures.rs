974be8122dcf5fc67285b900a6d8a9b991b93d3c
skippi <jbtcao@gmail.com>
2020-07-13 23:15:18 -0500

    Added explanation for daily_temperatures

--- src/daily_temperatures.rs
+++ src/daily_temperatures.rs
@@ -1,3 +1,54 @@
+/// This algorithm is a bit tricky to explain. For this explanation, let's use this example input:
+///
+/// [68, 69, 71, 70, 69, 72, 69, 68, 70]
+///
+/// Since 68 < 69 < 71, it's fairly clear that 68 and 69 need to wait for only 1 day to reach a
+/// hotter temperature. This isn't too bad.
+///
+/// However, 71 must wait 3 days to reach a hotter temperature. Reaching this conclusion can be
+/// done through a naive O(n^2) method, but can it be done with lower time complexity?
+///
+/// Spoiler: it can be, but we should explore the data format to understand why. Let's start by
+/// listing out the next hottest days for each temperature in order:
+///
+/// 68 -> 69
+/// 69 -> 71
+/// 71 -> 72
+/// 70 -> 72
+/// 69 -> 72
+/// 72 -> None
+/// 69 -> 70
+/// 68 -> 70
+/// 70 -> None
+///
+/// It appears that some of the temperatures share the same next hottest temperature (EG: 71, 70,
+/// and 69 share 72). If we can find a way to store these same temperatures (like 72 and 70) to
+/// assign them, we'd be able to solve our problem much more efficiently. Let's start by addressing
+/// some patterns to gauge our problem better.
+///
+/// One pattern to notice is that the next hottest temperature always occurs afterwards for each
+/// day. This hints at a small suggestion: it might be better to iterate from the end of the input.
+///
+/// Something else to notice is that the next hottest temperature never occurs on the day of;
+/// rather that it always happens afterwards. This is most prevalent with 72 and 70.
+///
+/// Given this information, there is a small link to our data format. Let's look at [69, 68, 70].
+/// Say we iterated from end to beginning. Let's make it a goal to figure out each day's next
+/// hottest temperature. To do this for these three days, we essentially discarded [68, 69] to
+/// reach 70 for all of these. Modeling this sort of discarding can be done with a stack structure.
+///
+/// 70 -> []        | None
+///    -> [70]
+/// 68 -> [70]      | 70
+///    -> [70, 68]
+/// 69 -> [70, 68]
+///    -> [70]      | 70
+///    -> [70, 69]
+/// 72 -> [70, 69]
+///    -> [70]
+///    -> []        | None
+///
+/// This stack-based algorithm leads us to our non-naive solution:
 #[allow(dead_code)]
 fn daily_temperatures(t: Vec<i32>) -> Vec<i32> {
     let mut result = vec![0; t.len()];
