2d898909336097784ea85c6dd5a049008150279c
skippi <jbtcao@gmail.com>
2020-07-30 15:17:54 -0500

    Added reverse_list solution

--- src/reverse_list.rs
+++ src/reverse_list.rs
@@ -13,8 +13,14 @@ impl ListNode {
 }
 
 #[allow(dead_code)]
-fn reverse_list(_head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
-    None
+fn reverse_list(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    let mut result: Option<Box<ListNode>> = None;
+    while head.is_some() {
+        let val = head.as_ref().map(|n| n.val).unwrap();
+        result = Some(Box::new(ListNode { val, next: result }));
+        head = head.and_then(|node| node.next);
+    }
+    result
 }
 
 #[cfg(test)]
@@ -22,22 +28,29 @@ mod tests {
     use super::*;
 
     macro_rules! list {
-        [] => {None};
         [$($x:expr), *] => {
             {
-                let mut result: Option<Box<ListNode>> = None;
-                let mut current = &mut result;
-                $(
-                    *current = Some($x);
-                    current = current.unwrap().next;
-                )*
-                result
+                make_list(&[$($x,)*])
             }
         }
+    }
+
+    fn make_list(data: &[i32]) -> Option<Box<ListNode>> {
+        let mut result: Option<Box<ListNode>> = None;
+        for &n in data.iter().rev() {
+            result = Some(Box::new(ListNode {
+                val: n,
+                next: result,
+            }))
+        }
+        result
     }
 
     #[test]
     fn test_inorder_traversal() {
         assert_eq!(reverse_list(list![]), list![]);
+        assert_eq!(reverse_list(list![1]), list![1]);
+        assert_eq!(reverse_list(list![1, 2]), list![2, 1]);
+        assert_eq!(reverse_list(list![1, 2, 3]), list![3, 2, 1]);
     }
 }
