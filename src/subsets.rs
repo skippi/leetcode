c8b21aea783063fc9fbe9f9311a33209a0e75b24
skippi <jbtcao@gmail.com>
2020-07-30 18:12:24 -0500

    Added subsets solution

--- src/subsets.rs
+++ src/subsets.rs
@@ -1,6 +1,32 @@
 #[allow(dead_code)]
-fn subsets(_nums: Vec<i32>) -> Vec<Vec<i32>> {
-    vec![]
+fn subsets(nums: Vec<i32>) -> Vec<Vec<i32>> {
+    (0..nums.len() + 1)
+        .flat_map(|k| combinations(&nums, k))
+        .collect()
+}
+
+fn combinations(nums: &Vec<i32>, k: usize) -> Vec<Vec<i32>> {
+    let mut result = vec![];
+    dfs(nums, k, 0, &mut vec![], &mut result);
+    result
+}
+
+fn dfs(
+    nums: &Vec<i32>,
+    k: usize,
+    start: usize,
+    current: &mut Vec<i32>,
+    result: &mut Vec<Vec<i32>>,
+) {
+    if current.len() == k {
+        result.push(current.clone());
+        return;
+    }
+    for i in start..nums.len() {
+        current.push(nums[i]);
+        dfs(nums, k, i + 1, current, result);
+        current.pop();
+    }
 }
 
 #[cfg(test)]
@@ -9,6 +35,11 @@ mod tests {
 
     #[test]
     fn test_subsets() {
-        assert_eq!(subsets(vec![]), Vec::<Vec<i32>>::new());
+        assert_eq!(subsets(vec![]), vec![vec![]]);
+        assert_eq!(subsets(vec![1]), vec![vec![], vec![1]]);
+        assert_eq!(
+            subsets(vec![1, 2]),
+            vec![vec![], vec![1], vec![2], vec![1, 2]]
+        );
     }
 }
