e1d1257be292f058705cb581366dd62a521171b9
skippi <jbtcao@gmail.com>
2020-07-29 23:29:47 -0500

    Added generate_parenthesis solution

--- src/generate_parenthesis.rs
+++ src/generate_parenthesis.rs
@@ -1,6 +1,25 @@
 #[allow(dead_code)]
-fn generate_parenthesis(_n: i32) -> Vec<String> {
-    vec!["()".to_string()]
+fn generate_parenthesis(n: i32) -> Vec<String> {
+    fn build_parens(n: i32) -> Vec<String> {
+        match n {
+            0 => vec![String::from("")],
+            _ => {
+                let mut result = vec![];
+                for c in 0..n {
+                    for left in build_parens(n - 1 - c) {
+                        for right in build_parens(c) {
+                            result.push(format!("({}){}", left, right));
+                        }
+                    }
+                }
+                result
+            }
+        }
+    }
+    match n {
+        0 => vec![],
+        _ => build_parens(n),
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +27,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_inorder_traversal() {
-        assert_eq!(generate_parenthesis(1), vec!["()"])
+    fn test_generate_parenthesis() {
+        assert_eq!(generate_parenthesis(0), Vec::<String>::new());
+        assert_eq!(generate_parenthesis(1), vec!["()"]);
+        assert_eq!(generate_parenthesis(2), vec!["(())", "()()"]);
+        assert_eq!(
+            generate_parenthesis(3),
+            vec!["((()))", "(()())", "(())()", "()(())", "()()()"]
+        );
+        assert_eq!(
+            generate_parenthesis(4),
+            vec![
+                "(((())))", "((()()))", "((())())", "(()(()))", "(()()())", "((()))()", "(()())()",
+                "(())(())", "(())()()", "()((()))", "()(()())", "()(())()", "()()(())", "()()()()"
+            ]
+        );
     }
 }
