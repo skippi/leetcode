904cd1841115ee16cda6903ec1f346c7d30b94f1
skippi <jbtcao@gmail.com>
2020-08-12 19:27:41 -0500

    Removed unnecessary literal

--- src/count_substrings.rs
+++ src/count_substrings.rs
@@ -3,7 +3,7 @@ fn count_substrings(s: String) -> i32 {
     let mut result = 0;
     let n = s.len() as i32;
     let bytes = s.as_bytes();
-    for center in 0i32..(2 * n - 1) {
+    for center in 0..(2 * n - 1) {
         let mut left = center / 2;
         let mut right = left + center % 2;
         while 0 <= left && right < n && bytes[left as usize] == bytes[right as usize] {
